87872ebfe4b8e1396e70a6884bfbb18be56a6310
snipe <specialsnipe@mail.ru>
2022-06-26 17:47:57 +0500

    rename function

--- src/Even.php
+++ src/Even.php
@@ -5,7 +5,7 @@ namespace Snipe\Even;
 use function cli\line;
 use function cli\prompt;
 
-function begin()
+function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
