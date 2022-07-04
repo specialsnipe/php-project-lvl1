1bee5bfded31ddea1b922c79338fd7a292fdf514
snipe <specialsnipe@mail.ru>
2022-07-04 13:06:59 +0500

    modify line break

--- src/Games/Cli.php
+++ src/Games/Cli.php
@@ -1,13 +1,1 @@
-<?php
-
-namespace Snipe\Cli;
-
-use function cli\line;
-use function cli\prompt;
-
-function run()
-{
-    line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-}
+<?phpnamespace Snipe\Cli;use function cli\line;use function cli\prompt;function run(){    line('Welcome to the Brain Games!');    $name = prompt('May I have your name?');    line("Hello, %s!", $name);}
--- src/Games/Even.php
+++ src/Games/Even.php
@@ -1,26 +1,1 @@
-<?php
-
-namespace Snipe\Even;
-
-use function Snipe\Engine\run;
-
-use const Snipe\Engine\ROUNDS_COUNT;
-
-const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-
-function isEven($number)
-{
-    return $number % 2 === 0;
-}
-
-function runGame()
-{
-    $gameData = [];
-    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
-        $number = mt_rand(1, 50);
-        $rightAnswer = isEven($number) ? 'yes' : 'no';
-        $gameData[] = [$number, $rightAnswer];
-    }
-    run(DESCRIPTION, $gameData);
-}
+<?phpnamespace Snipe\Even;use function Snipe\Engine\run;use const Snipe\Engine\ROUNDS_COUNT;const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';function isEven($number){    return $number % 2 === 0;}function runGame(){    $gameData = [];    for ($i = 0; $i < ROUNDS_COUNT; $i++) {        $number = mt_rand(1, 50);        $rightAnswer = isEven($number) ? 'yes' : 'no';        $gameData[] = [$number, $rightAnswer];    }    run(DESCRIPTION, $gameData);}
